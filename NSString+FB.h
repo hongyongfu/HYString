884b6dc65875c10522a8eed44a84c6cf91c158ef
fuhongyong <446872912@qq.com>
2019-04-22 10:00:56 +0800

    first commit

---
+++ NSString+FB.h
@@ -0,0 +1,17 @@
+//
+//  NSString+FB.h
+//  FutureBazaar
+//
+//  Created by hongyong fu on 2019/1/23.
+//  Copyright © 2019年 Sibu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (FB)
+
+
+
+- (NSString *)bandCardSafetyNumber;
+
+@end
---
+++ NSString+FB.m
@@ -0,0 +1,24 @@
+//
+//  NSString+FB.m
+//  FutureBazaar
+//
+//  Created by hongyong fu on 2019/1/23.
+//  Copyright © 2019年 Sibu. All rights reserved.
+//
+
+#import "NSString+FB.h"
+@implementation NSString (FB)
+
+- (NSString *)bandCardSafetyNumber{
+
+    NSString *regPattern = @"/^(d{4})d+(d{4})$/";
+    NSString *replacement = @" **** **** ";
+    
+    NSRegularExpression *regExp = [[NSRegularExpression alloc] initWithPattern:regPattern options:0 error:nil];
+    NSString *result = [regExp stringByReplacingMatchesInString:self options:NSMatchingReportProgress range:NSMakeRange(0, self.length) withTemplate:replacement];
+
+    return result;
+}
+
+
+@end
